ef37226a1c23d9c15ac6d4a3c4a647918258c944
clown <tt.clown@gmail.com>
2015-09-05 18:59:33 +0900

    initial commit.

---
+++ Page.cs
@@ -0,0 +1,95 @@
+﻿/* ------------------------------------------------------------------------- */
+///
+/// Page.cs
+/// 
+/// Copyright (c) 2010 CubeSoft, Inc.
+/// 
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///  http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/* ------------------------------------------------------------------------- */
+using System;
+using Size = System.Drawing.Size;
+
+namespace Cube.Pdf
+{
+    /* --------------------------------------------------------------------- */
+    ///
+    /// Page
+    /// 
+    /// <summary>
+    /// PDF のページを表すクラスです。
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    public class Page : IPage
+    {
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Type
+        /// 
+        /// <summary>
+        /// オブジェクトの種類を取得します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public PageType Type
+        {
+            get { return PageType.Pdf; }
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// FileName
+        /// 
+        /// <summary>
+        /// リソースとなる PDF ファイルのファイル名 (パス) を取得または
+        /// 設定します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public string FileName { get; set; }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Path
+        /// 
+        /// <summary>
+        /// リソースとなる PDF ファイルのパスワードを取得または設定します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public string Password { get; set; }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Path
+        /// 
+        /// <summary>
+        /// リソースとなる PDF ファイルのページ番号を取得または設定します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public uint PageNumber { get; set; }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Size
+        /// 
+        /// <summary>
+        /// リソースとなる PDF ファイルのページ番号を取得または設定します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public Size Size { get; set; }
+    }
+}
---
+++ PageType.cs
@@ -0,0 +1,39 @@
+﻿/* ------------------------------------------------------------------------- */
+///
+/// PageType.cs
+/// 
+/// Copyright (c) 2010 CubeSoft, Inc.
+/// 
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///  http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/* ------------------------------------------------------------------------- */
+using System;
+
+namespace Cube.Pdf
+{
+    /* --------------------------------------------------------------------- */
+    ///
+    /// PageType
+    /// 
+    /// <summary>
+    /// Page オブジェクトの種類を定義した列挙型です。
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    public enum PageType : uint
+    {
+        Pdf,
+        Image,
+        Unknown = -1
+    }
+}
