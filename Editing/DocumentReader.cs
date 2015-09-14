a399926bbb1660e6a6ac8f37932d73d8e194edc9
clown <tt.clown@gmail.com>
2015-09-14 19:51:32 +0900

    DocumentReader のひな型を追加

---
+++ Editing/DocumentReader.cs
@@ -0,0 +1,209 @@
+﻿/* ------------------------------------------------------------------------- */
+///
+/// DocumentReader.cs
+/// 
+/// Copyright (c) 2010 CubeSoft, Inc.
+/// 
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///  http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/* ------------------------------------------------------------------------- */
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Cube.Pdf.Editing
+{
+    /* --------------------------------------------------------------------- */
+    ///
+    /// Cube.Pdf.DocumentReader
+    /// 
+    /// <summary>
+    /// PDF ファイルを読み込んで各種情報を保持するためのクラスです。
+    /// </summary>
+    /// 
+    /// <remarks>
+    /// このクラスは iTextSharp を用いて PDF ファイルの解析を行います。
+    /// </remarks>
+    ///
+    /* --------------------------------------------------------------------- */
+    public class DocumentReader : IDocumentReader
+    {
+        #region Constructors and destructors
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// ~DocumentReader
+        /// 
+        /// <summary>
+        /// オブジェクトを破棄します。
+        /// </summary>
+        /// 
+        /// <remarks>
+        /// クラスで必要な終了処理は、デストラクタではなく Dispose メソッド
+        /// に記述して下さい。
+        /// </remarks>
+        ///
+        /* ----------------------------------------------------------------- */
+        ~DocumentReader()
+        {
+            Dispose(false);
+        }
+
+        #endregion
+
+        #region Properties
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// FileName
+        /// 
+        /// <summary>
+        /// リソースとなる PDF ファイルのファイル名 (パス) を取得または
+        /// 設定します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public string FileName { get; } = string.Empty;
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Metadata
+        /// 
+        /// <summary>
+        /// PDF ファイルに関するメタデータを取得します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public Metadata Metadata { get; } = new Metadata();
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Metadata
+        /// 
+        /// <summary>
+        /// PDF ファイルに関する暗号化情報を取得します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public Encryption Encryption { get; } = new Encryption();
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// EncryptionStatus
+        /// 
+        /// <summary>
+        /// 暗号化されている PDF ファイルへのアクセス状態を取得します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public EncryptionStatus EncryptionStatus { get; } = EncryptionStatus.NotEncrypted;
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Pages
+        /// 
+        /// <summary>
+        /// PDF ファイルの各ページ情報へアクセスするための反復子を取得します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public ICollection<IPage> Pages { get; } = new List<IPage>();
+
+        #endregion
+
+        #region Methods
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// OpenAsync
+        /// 
+        /// <summary>
+        /// PDF ファイルを非同期で開きます。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public Task OpenAsync(string path, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Close
+        /// 
+        /// <summary>
+        /// 現在、開いている PDF ファイルを閉じます。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public void Close()
+        {
+            throw new NotImplementedException();
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Dispose
+        /// 
+        /// <summary>
+        /// オブジェクトを破棄する際に必要な終了処理を実行します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// GetPage
+        /// 
+        /// <summary>
+        /// 指定されたページ番号に対応するページ情報を取得します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public Page GetPage(int pagenum)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region Override methods
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Dispose
+        /// 
+        /// <summary>
+        /// オブジェクトを破棄する際に必要な終了処理を実行します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed) return;
+            _disposed = true;
+            if (disposing) Close();
+        }
+
+        #endregion
+
+        #region Fields
+        private bool _disposed = false;
+        #endregion
+    }
+}
