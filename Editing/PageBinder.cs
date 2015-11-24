5c363fea4411a12216142842e2e2fa5a61332492
clown <tt.clown@gmail.com>
2015-11-24 13:33:47 +0900

    PageBinder を追加

---
+++ Editing/PageBinder.cs
@@ -0,0 +1,154 @@
+﻿/* ------------------------------------------------------------------------- */
+///
+/// PageBinder.cs
+///
+/// Copyright (c) 2010 CubeSoft, Inc.
+///
+/// This program is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU Affero General Public License as published
+/// by the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU Affero General Public License for more details.
+///
+/// You should have received a copy of the GNU Affero General Public License
+/// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+///
+/* ------------------------------------------------------------------------- */
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using iTextSharp.text.pdf;
+using IoEx = System.IO;
+
+namespace Cube.Pdf.Editing
+{
+    /* --------------------------------------------------------------------- */
+    ///
+    /// PageBinder
+    /// 
+    /// <summary>
+    /// 複数の PDF ファイルのページの一部、または全部をまとめて一つの
+    /// PDF ファイルにするためのクラスです。
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    public class PageBinder : IDocumentWriter
+    {
+        #region Constructors
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// PageBinder
+        /// 
+        /// <summary>
+        /// オブジェクトを初期化します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public PageBinder() { }
+
+        #endregion
+
+        #region Properties
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Metadata
+        /// 
+        /// <summary>
+        /// PDF ファイルのメタデータを取得または設定します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public Metadata Metadata { get; set; } = new Metadata();
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Encryption
+        /// 
+        /// <summary>
+        /// 暗号化に関する情報をを取得または設定します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public Encryption Encryption { get; set; } = new Encryption();
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Pages
+        /// 
+        /// <summary>
+        /// PDF ファイルの各ページ情報を取得または設定します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public ICollection<IPage> Pages { get; } = new List<IPage>();
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// UseSmartCopy
+        /// 
+        /// <summary>
+        /// ファイルサイズを抑えるための結合方法を使用するかどうかを取得、
+        /// または設定します。
+        /// </summary>
+        /// 
+        /// <remarks>
+        /// 通常時には iTextSharp の PdfCopy クラスを用いて結合を行って
+        /// いるが、このクラスは複数の PDF ファイルが同じフォントを使用
+        /// していたとしても別々のものとして扱うため、フォント情報が重複して
+        /// ファイルサイズが増大する場合がある。
+        /// 
+        /// この問題を解決したものとして PdfSmartCopy クラスが存在する。
+        /// ただし、複雑な注釈が保存されている PDF を結合する際に使用した
+        /// 場合、（別々として扱わなければならないはずの）情報が共有されて
+        /// しまい、注釈の構造が壊れてしまう問題が確認されている。
+        /// </remarks>
+        ///
+        /* ----------------------------------------------------------------- */
+        public bool UseSmartCopy { get; set; } = true;
+
+        #endregion
+
+        #region Methods
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Reset
+        /// 
+        /// <summary>
+        /// 初期状態にリセットします。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public void Reset()
+        {
+            Metadata   = new Metadata();
+            Encryption = new Encryption();
+            Pages.Clear();
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Save
+        /// 
+        /// <summary>
+        /// メンバ変数が保持している、メタデータ、暗号化に関する情報、
+        /// 各ページ情報に基づいた PDF ファイルを指定されたパスに保存
+        /// します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public void Save(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
