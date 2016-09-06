bba2dbd1dbab0328d6c05b392a246d1ecf675287
clown <tt.clown@gmail.com>
2016-09-06 14:35:56 +0900

    MuPDF に対する操作を追加

--- Libraries/Drawing/Operations/MuPdf.cs
+++ Libraries/Drawing/Operations/MuPdf.cs
@@ -20,6 +20,7 @@
 /* ------------------------------------------------------------------------- */
 using System;
 using System.Drawing;
+using System.Drawing.Imaging;
 
 namespace Cube.Pdf.Drawing.MuPdf
 {
@@ -34,6 +35,8 @@ namespace Cube.Pdf.Drawing.MuPdf
     /* --------------------------------------------------------------------- */
     internal static class Operations
     {
+        #region Methods
+
         /* ----------------------------------------------------------------- */
         ///
         /// CreatePage
@@ -50,13 +53,166 @@ namespace Cube.Pdf.Drawing.MuPdf
             var width  = NativeMethods.GetWidth(core);
             var height = NativeMethods.GetHeight(core);
 
-            var dest = new Page();
-            dest.File = file;
-            dest.Number = pagenum;
-            dest.Size = new Size(width, height);
-            dest.Rotation = NativeMethods.GetRotation(core);
-            dest.Resolution = new Point(72, 72);
+            return new Page
+            {
+                File        = file,
+                Number      = pagenum,
+                Size        = new Size(width, height),
+                Rotation    = NativeMethods.GetRotation(core),
+                Resolution  = new Point(72, 72)
+            };
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// CreateMetadata
+        /// 
+        /// <summary>
+        /// Metadata オブジェクトを生成します。
+        /// </summary>
+        /// 
+        /* ----------------------------------------------------------------- */
+        public static Metadata CreateMetadata(this IntPtr core)
+            => new Metadata
+        {
+            Title    = NativeMethods.GetStringProperty(core, NativeMethods.GetTitle),
+            Author   = NativeMethods.GetStringProperty(core, NativeMethods.GetAuthor),
+            Subtitle = NativeMethods.GetStringProperty(core, NativeMethods.GetSubject),
+            Keywords = NativeMethods.GetStringProperty(core, NativeMethods.GetKeywords),
+            Creator  = NativeMethods.GetStringProperty(core, NativeMethods.GetCreator),
+            Producer = NativeMethods.GetStringProperty(core, NativeMethods.GetProducer)
+        };
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// CreateEncryption
+        /// 
+        /// <summary>
+        /// Encryption オブジェクトを生成します。
+        /// </summary>
+        /// 
+        /* ----------------------------------------------------------------- */
+        public static Encryption CreateEncryption(this IntPtr core, string password)
+        {
+            var dest = new Encryption
+            {
+                IsEnabled     = string.IsNullOrEmpty(password),
+                OwnerPassword = password
+            };
+
+            var basic = NativeMethods.HasPrintPermission(core);
+            var high  = NativeMethods.HasHighResolutionPrintPermission(core);
+
+            dest.Permission.Assembly          = ConvertTo(NativeMethods.HasAssemblePermission(core));
+            dest.Permission.ModifyContents    = ConvertTo(NativeMethods.HasEditPermission(core));
+            dest.Permission.CopyContents      = ConvertTo(NativeMethods.HasCopyPermission(core));
+            dest.Permission.InputFormFields   = ConvertTo(NativeMethods.HasFillFormPermission(core));
+            dest.Permission.ModifyAnnotations = ConvertTo(NativeMethods.HasAnnotatePermission(core));
+            dest.Permission.Accessibility     = ConvertTo(NativeMethods.HasAccessibilityPermission(core));
+            dest.Permission.Printing          = high  ? PermissionMethod.Allow :
+                                                basic ? PermissionMethod.Restrict :
+                                                        PermissionMethod.Deny;
+
+            // dest.ExtractPage    = ConvertTo(???);
+            // dest.Signature      = ConvertTo(???);
+            // dest.TemplatePage   = ConvertTo(???);
+
             return dest;
         }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// CreateImage
+        /// 
+        /// <summary>
+        /// Image オブジェクトを生成します。
+        /// </summary>
+        /// 
+        /* ----------------------------------------------------------------- */
+        public static Image CreateImage(this IntPtr core, Page page, double power)
+        {
+            var width  = 0;
+            var height = 0;
+            var length = 0;
+            var raw    = NativeMethods.GetBitmap(
+                core,
+                out width,
+                out height,
+                (float)(power * page.Resolution.X),
+                (float)(power * page.Resolution.Y),
+                0,
+                0,
+                false,
+                false,
+                out length,
+                0
+            );
+
+            if (raw == IntPtr.Zero) return null;
+
+            var dest = ConvertTo(raw, width, height);
+            NativeMethods.FreeRenderedPage(core);
+            return dest;
+        }
+
+        #endregion
+
+        #region Others
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// ConvertTo
+        /// 
+        /// <summary>
+        /// C で生成されたイメージを Image オブジェクトに変換します。
+        /// </summary>
+        /// 
+        /* ----------------------------------------------------------------- */
+        private static unsafe Image ConvertTo(IntPtr raw, int width, int height)
+        {
+            var dest = new Bitmap(width, height, PixelFormat.Format24bppRgb);
+            var bits = dest.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadWrite, dest.PixelFormat);
+            var ptrSrc  = (byte*)raw;
+            var ptrDest = (byte*)bits.Scan0;
+            for (var y = 0; y < height; y++)
+            {
+                var pl = ptrDest;
+                var sl = ptrSrc;
+                for (var x = 0; x < width; x++)
+                {
+                    // Swap these here instead of in MuPDF
+                    // because most pdf images will be rgb or cmyk.
+                    // Since we are going through the pixels one by one
+                    // anyway swap here to save a conversion from rgb to bgr.
+
+                    pl[2] = sl[0]; //b-r
+                    pl[1] = sl[1]; //g-g
+                    pl[0] = sl[2]; //r-b
+                    //pl[3] = sl[3]; //alpha
+                    pl += 3;
+                    sl += 4;
+                }
+                ptrDest += bits.Stride;
+                ptrSrc += width * 4;
+            }
+            dest.UnlockBits(bits);
+            return dest;
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// ConvertTo
+        /// 
+        /// <summary>
+        /// 真偽値を PermissionMethod オブジェクトに変換します。
+        /// </summary>
+        /// 
+        /* ----------------------------------------------------------------- */
+        private static PermissionMethod ConvertTo(bool value)
+            => value ?
+               PermissionMethod.Allow :
+               PermissionMethod.Deny;
+
+        #endregion
     }
 }
