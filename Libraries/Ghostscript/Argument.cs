928b1694c5f0255f6622e6ed48c9e60e4369c71b
clown <tt.clown@gmail.com>
2018-06-11 19:29:05 +0900

    Rename some classes.

---
+++ Libraries/Ghostscript/Argument.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Text;
+
+namespace Cube.Pdf.Ghostscript
+{
+    /* --------------------------------------------------------------------- */
+    ///
+    /// Argument
+    ///
+    /// <summary>
+    /// Interpreter に指定可能な引数を表すクラスです。
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    public class Argument
+    {
+        #region Properties
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Key
+        ///
+        /// <summary>
+        /// キーを取得または設定します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public string Key { get; set; }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Value
+        ///
+        /// <summary>
+        /// 値を取得または設定します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public string Value { get; set; }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// KeyPrefix
+        ///
+        /// <summary>
+        /// キーの接頭辞を取得または設定します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public char KeyPrefix { get; set; } = 'd';
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// ValuePrefix
+        ///
+        /// <summary>
+        /// 値の接頭辞を取得または設定します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public char ValuePrefix { get; set; } = '/';
+
+        #endregion
+
+        #region Methods
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// ToString
+        ///
+        /// <summary>
+        /// 引数を表す文字列を取得します。
+        /// </summary>
+        ///
+        /// <returns>文字列</returns>
+        ///
+        /* ----------------------------------------------------------------- */
+        public override string ToString()
+        {
+            if (string.IsNullOrEmpty(Key)) throw new ArgumentException(nameof(Key));
+
+            var sb = new StringBuilder();
+            sb.Append($"-{KeyPrefix}{Key}");
+            if (!string.IsNullOrEmpty(Value)) sb.Append($"={ValuePrefix}{Value}");
+
+            return base.ToString();
+        }
+
+        #endregion
+    }
+}
---
+++ Libraries/Ghostscript/Parameters/Format.cs
@@ -0,0 +1,89 @@
+﻿/* ------------------------------------------------------------------------- */
+//
+// Copyright (c) 2010 CubeSoft, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+/* ------------------------------------------------------------------------- */
+namespace Cube.Pdf.Ghostscript
+{
+    /* --------------------------------------------------------------------- */
+    ///
+    /// Format
+    ///
+    /// <summary>
+    /// Ghostscript で変換可能なフォーマットを定義した列挙型です。
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    public enum Format
+    {
+        /// <summary>Text</summary>
+        Text,
+        /// <summary>PostScript (PS)</summary>
+        PostScript,
+        /// <summary>EPS</summary>
+        Eps,
+        /// <summary>PDF</summary>
+        Pdf,
+        /// <summary>PSD (RGB)</summary>
+        Psd,
+        /// <summary>PSD (CMYK)</summary>
+        PsdCmyk,
+        /// <summary>JPEG (24bpp RGB)</summary>
+        Jpeg24bppRGB,
+        /// <summary>JPEG (32bpp CMYK)</summary>
+        Jpeg32bppCMYK,
+        /// <summary>JPEG (8bpp Grayscale)</summary>
+        Jpeg8bppGrayscale,
+        /// <summary>PNG (24bpp RGB)</summary>
+        Png24bppRGB,
+        /// <summary>PNG (32bpp ARGB)</summary>
+        Png32bppArgb,
+        /// <summary>PNG (16 colors)</summary>
+        Png4bppIndexed,
+        /// <summary>PNG (256 colors)</summary>
+        Png8bppIndexed,
+        /// <summary>PNG (8bpp Grayscale)</summary>
+        Png8bppGrayscale,
+        /// <summary>PNG (Monochrome)</summary>
+        Png1bppMonochrome,
+        /// <summary>BMP (24bpp RGB)</summary>
+        Bmp24bppRgb,
+        /// <summary>BMP (32bpp RGB)</summary>
+        Bmp32bppArgb,
+        /// <summary>BMP (16 colors)</summary>
+        Bmp4bppIndexed,
+        /// <summary>BMP (256 colors)</summary>
+        Bmp8bppIndexed,
+        /// <summary>BMP (8bpp Grayscale)</summary>
+        Bmp8bppGrayscale,
+        /// <summary>BMP (Monochrome)</summary>
+        Bmp1bppMonochrome,
+        /// <summary>TIFF (12bpp RGB)</summary>
+        Tiff12bppRgb,
+        /// <summary>TIFF (24bpp RGB)</summary>
+        Tiff24bppRgb,
+        /// <summary>TIFF (48bpp RGB)</summary>
+        Tiff48bppRgb,
+        /// <summary>TIFF (32bpp CMYK)</summary>
+        Tiff32bppCmyk,
+        /// <summary>TIFF (64bpp CMYK)</summary>
+        Tiff64bppCmyk,
+        /// <summary>TIFF (8bpp Grayscale)</summary>
+        Tiff8bppGrayscale,
+        /// <summary>TIFF (Monochrome)</summary>
+        Tiff1bppMonochrome,
+    }
+}
