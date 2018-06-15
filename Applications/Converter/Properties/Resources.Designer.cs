74662b7cf9acc464452591dc17fe19587cdcccc1
clown <tt.clown@gmail.com>
2018-06-15 12:03:46 +0900

    Add logo image.

--- Applications/Converter/Properties/Resources.Designer.cs
+++ Applications/Converter/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace Cube.Pdf.App.Converter.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   型 System.Drawing.Bitmap のローカライズされたリソースを検索します。
+        /// </summary>
+        internal static System.Drawing.Bitmap Logo {
+            get {
+                object obj = ResourceManager.GetObject("Logo", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
