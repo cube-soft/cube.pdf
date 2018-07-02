9a00c9384232f53ec4b01efe7dd886716328bd2e
clown <tt.clown@gmail.com>
2018-07-02 18:36:59 +0900

    Update tests.

--- Tests/Itext/DocumentReaderTest.cs
+++ Tests/Itext/DocumentReaderTest.cs
@@ -19,8 +19,6 @@
 using Cube.FileSystem.Tests;
 using Cube.Pdf.Itext;
 using NUnit.Framework;
-using System;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace Cube.Pdf.Tests.Itext
@@ -40,70 +38,6 @@ namespace Cube.Pdf.Tests.Itext
         #region Tests
 
         #region Open
-
-        /* ----------------------------------------------------------------- */
-        ///
-        /// Open
-        ///
-        /// <summary>
-        /// PDF ファイルを開くテストを実行します。
-        /// </summary>
-        ///
-        /* ----------------------------------------------------------------- */
-        [TestCase("SampleRotation.pdf",       "",         true,  ExpectedResult = 9)]
-        [TestCase("SamplePassword.pdf",       "password", true,  ExpectedResult = 2)]
-        [TestCase("SamplePassword.pdf",       "view",     false, ExpectedResult = 2)]
-        [TestCase("SamplePasswordAes256.pdf", "password", true,  ExpectedResult = 9)]
-        public int Open(string filename, string password, bool fullaccess)
-        {
-            using (var reader = Create(filename, password))
-            {
-                var dest = reader.File as PdfFile;
-
-                Assert.That(dest.Name,         Is.EqualTo(filename));
-                Assert.That(dest.FullName,     Is.EqualTo(GetExamplesWith(filename)));
-                Assert.That(dest.Password,     Is.EqualTo(password));
-                Assert.That(dest.FullAccess,   Is.EqualTo(fullaccess));
-                Assert.That(dest.Length,       Is.AtLeast(1));
-                Assert.That(dest.Resolution.X, Is.EqualTo(72.0f));
-                Assert.That(dest.Resolution.Y, Is.EqualTo(72.0f));
-                Assert.That(dest.Count,        Is.EqualTo(reader.Pages.Count()));
-
-                return dest.Count;
-            }
-        }
-
-        /* ----------------------------------------------------------------- */
-        ///
-        /// Encryption
-        ///
-        /// <summary>
-        /// Encryption オブジェクトの内容を確認します。
-        /// </summary>
-        ///
-        /* ----------------------------------------------------------------- */
-        [TestCaseSource(nameof(Open_Encryption_TestCases))]
-        public void Open_Encryption(string filename, string password, Encryption expected)
-        {
-            using (var reader = Create(filename, password))
-            {
-                var dest = reader.Encryption;
-                Assert.That(dest.Enabled,          Is.EqualTo(expected.Enabled),          nameof(dest.Enabled));
-                Assert.That(dest.OpenWithPassword, Is.EqualTo(expected.OpenWithPassword), nameof(dest.OpenWithPassword));
-                Assert.That(dest.Method,           Is.EqualTo(expected.Method),           nameof(dest.Method));
-                Assert.That(dest.UserPassword,     Is.EqualTo(expected.UserPassword),     nameof(dest.UserPassword));
-
-                var dpm = dest.Permission;
-                var epm = expected.Permission;
-                Assert.That(dpm.Accessibility,     Is.EqualTo(epm.Accessibility),     nameof(dpm.Accessibility));
-                Assert.That(dpm.Assemble,          Is.EqualTo(epm.Assemble),          nameof(dpm.Assemble));
-                Assert.That(dpm.CopyContents,      Is.EqualTo(epm.CopyContents),      nameof(dpm.CopyContents));
-                Assert.That(dpm.InputForms,  Is.EqualTo(epm.InputForms),  nameof(dpm.InputForms));
-                Assert.That(dpm.ModifyAnnotations, Is.EqualTo(epm.ModifyAnnotations), nameof(dpm.ModifyAnnotations));
-                Assert.That(dpm.ModifyContents,    Is.EqualTo(epm.ModifyContents),    nameof(dpm.ModifyContents));
-                Assert.That(dpm.Print,             Is.EqualTo(epm.Print),             nameof(dpm.Print));
-            }
-        }
 
         /* ----------------------------------------------------------------- */
         ///
@@ -142,41 +76,6 @@ namespace Cube.Pdf.Tests.Itext
             }
         }
 
-        /* ----------------------------------------------------------------- */
-        ///
-        /// Open_BadPassword_Throws
-        ///
-        /// <summary>
-        /// 間違ったパスワードを入力して PDF ファイルを開こうとするテストを
-        /// 実行します。
-        /// </summary>
-        ///
-        /* ----------------------------------------------------------------- */
-        [Test]
-        public void Open_BadPassword_Throws() => Assert.That(() =>
-        {
-            var filename = "SamplePassword.pdf";
-            var password = "bad-password-string";
-            using (var _ = Create(filename, password)) { }
-        }, Throws.TypeOf<EncryptionException>());
-
-        /* ----------------------------------------------------------------- */
-        ///
-        /// Open_PasswordCancel_Throws
-        ///
-        /// <summary>
-        /// パスワードの入力をキャンセルした時の挙動を確認します。
-        /// </summary>
-        ///
-        /* ----------------------------------------------------------------- */
-        [Test]
-        public void Open_PasswordCancel_Throws() => Assert.That(() =>
-        {
-            var src   = GetExamplesWith("SamplePassword.pdf");
-            var query = new Query<string>(e => e.Cancel = true);
-            using (var reader = new DocumentReader(src, query, IO)) { }
-        }, Throws.TypeOf<OperationCanceledException>());
-
         #endregion
 
         /* ----------------------------------------------------------------- */
@@ -193,61 +92,6 @@ namespace Cube.Pdf.Tests.Itext
         public int ExtractImages(string filename, int n)
         {
             using (var reader = Create(filename)) return reader.ExtractImages(n).Count();
-        }
-
-        #endregion
-
-        #region TestCases
-
-        /* ----------------------------------------------------------------- */
-        ///
-        /// Open_Encryption_TestCases
-        ///
-        /// <summary>
-        /// Open_Encryption のテストに必要なテストケースを取得します。
-        /// </summary>
-        ///
-        /* ----------------------------------------------------------------- */
-        public static IEnumerable<TestCaseData> Open_Encryption_TestCases
-        {
-            get
-            {
-                yield return new TestCaseData("SamplePassword.pdf", "password", new Encryption
-                {
-                    Method           = EncryptionMethod.Standard128,
-                    Enabled          = true,
-                    OpenWithPassword = true,
-                    UserPassword     = "view",
-                    Permission       = new Permission
-                    {
-                        Accessibility     = PermissionMethod.Deny,
-                        Assemble          = PermissionMethod.Allow,
-                        CopyContents      = PermissionMethod.Deny,
-                        InputForms        = PermissionMethod.Deny,
-                        ModifyAnnotations = PermissionMethod.Deny,
-                        ModifyContents    = PermissionMethod.Deny,
-                        Print             = PermissionMethod.Allow,
-                    }
-                });
-
-                yield return new TestCaseData("SamplePasswordAes256.pdf", "password", new Encryption
-                {
-                    Method           = EncryptionMethod.Aes256,
-                    Enabled          = true,
-                    OpenWithPassword = false, // true
-                    UserPassword     = "", // "view"
-                    Permission       = new Permission
-                    {
-                        Accessibility     = PermissionMethod.Allow,
-                        Assemble          = PermissionMethod.Allow,
-                        CopyContents      = PermissionMethod.Allow,
-                        InputForms        = PermissionMethod.Allow,
-                        ModifyAnnotations = PermissionMethod.Allow,
-                        ModifyContents    = PermissionMethod.Allow,
-                        Print             = PermissionMethod.Allow,
-                    }
-                });
-            }
         }
 
         #endregion
