c933f8b1c06b9768ea6adadbe1fce5fd79e393fe
clown <tt.clown@gmail.com>
2018-08-09 20:03:55 +0900

    Fix layout for RecentCollection component.

---
+++ Applications/Editor/Forms/Sources/Models/RecentItem.cs
@@ -0,0 +1,43 @@
+﻿/* ------------------------------------------------------------------------- */
+//
+// Copyright (c) 2010 CubeSoft, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+/* ------------------------------------------------------------------------- */
+namespace Cube.Pdf.App.Editor
+{
+    /* --------------------------------------------------------------------- */
+    ///
+    /// RecentItem
+    ///
+    /// <summary>
+    /// Provides properties of the resent used PDF file.
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    public class RecentItem
+    {
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Name
+        ///
+        /// <summary>
+        /// Gets filename of the recent item.
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public string Name { get; set; }
+    }
+}
