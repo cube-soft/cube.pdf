315bbf1a17111767b11166e04e942341caeb20d6
clown <tt.clown@gmail.com>
2018-10-16 13:21:12 +0900

    Refactoring.

---
+++ Applications/Editor/Forms/Views/Components/FileListControl.xaml.cs
@@ -0,0 +1,45 @@
+﻿/* ------------------------------------------------------------------------- */
+//
+// Copyright (c) 2010 CubeSoft, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+/* ------------------------------------------------------------------------- */
+using System.Windows.Controls;
+
+namespace Cube.Pdf.App.Editor
+{
+    /* --------------------------------------------------------------------- */
+    ///
+    /// FileListControl
+    ///
+    /// <summary>
+    /// Represents the code behind of the FileListControl.xaml.
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    public partial class FileListControl : UserControl
+    {
+        /* ----------------------------------------------------------------- */
+        ///
+        /// FileListControl
+        ///
+        /// <summary>
+        /// Initializes a new instance of the FileListControl class.
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public FileListControl() { InitializeComponent(); }
+    }
+}
