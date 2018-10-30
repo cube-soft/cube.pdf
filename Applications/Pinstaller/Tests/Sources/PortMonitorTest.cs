9fd65ce1bd6781be02f212a3702a3338a2cc7c3c
clown <tt.clown@gmail.com>
2018-10-30 17:41:52 +0900

    Add tests.

---
+++ Applications/Pinstaller/Tests/Sources/PortMonitorTest.cs
@@ -0,0 +1,82 @@
+﻿/* ------------------------------------------------------------------------- */
+//
+// Copyright (c) 2010 CubeSoft, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+/* ------------------------------------------------------------------------- */
+using Cube.Log;
+using Cube.Pdf.App.Pinstaller;
+using NUnit.Framework;
+using System.Linq;
+
+namespace Cube.Pdf.Tests.Pinstaller
+{
+    /* --------------------------------------------------------------------- */
+    ///
+    /// PortMonitorTest
+    ///
+    /// <summary>
+    /// Represents tests of the PortMonitor class.
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    [TestFixture]
+    class PortMonitorTest
+    {
+        #region Tests
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Create
+        ///
+        /// <summary>
+        /// Executes the test to create a new instance of the PortMonitor
+        /// class with the specified name.
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        [TestCase("Dummy Port", "",             ExpectedResult = false)]
+        [TestCase("Local Port", "localspl.dll", ExpectedResult = true )]
+        public bool Create(string name, string filename)
+        {
+            var src = new PortMonitor(name);
+
+            Assert.That(src.Name,        Is.EqualTo(name));
+            Assert.That(src.FileName,    Is.EqualTo(filename));
+            Assert.That(src.Environment, Is.Not.Null.And.Not.Empty);
+
+            return src.Exists;
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Create
+        ///
+        /// <summary>
+        /// Executes the test to create a new instance of the PortMonitor
+        /// class with the specified name.
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        [Test]
+        public void GetElements()
+        {
+            var src = PortMonitor.GetElements();
+            foreach (var e in src) this.LogDebug($"{e.Name}:{e.FileName} ({e.Environment})");
+            Assert.That(src.Count(), Is.AtLeast(2));
+        }
+
+        #endregion
+    }
+}
