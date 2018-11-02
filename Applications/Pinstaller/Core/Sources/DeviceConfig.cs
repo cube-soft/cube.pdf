19d1a0b1dcf9fe4666651fc40c6cb1f5e6c3f8b2
clown <tt.clown@gmail.com>
2018-11-02 15:21:42 +0900

    Add Installer class.

---
+++ Applications/Pinstaller/Core/Sources/DeviceConfig.cs
@@ -0,0 +1,154 @@
+﻿/* ------------------------------------------------------------------------- */
+//
+// Copyright (c) 2010 CubeSoft, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+/* ------------------------------------------------------------------------- */
+using System.Runtime.Serialization;
+
+namespace Cube.Pdf.App.Pinstaller
+{
+    /* --------------------------------------------------------------------- */
+    ///
+    /// DeviceConfig
+    ///
+    /// <summary>
+    /// Represents the configuration to install devices.
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    [DataContract]
+    public class DeviceConfig : ObservableProperty
+    {
+        #region Constructors
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// DeviceConfig
+        ///
+        /// <summary>
+        /// Initializes a new instance of the DeviceConfig class.
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public DeviceConfig() { Reset(); }
+
+        #endregion
+
+        #region Properties
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// PortMonitor
+        ///
+        /// <summary>
+        /// Gets or sets the configuration of the installing port monitor.
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public PortMonitorConfig PortMonitor
+        {
+            get => _portMonitor;
+            set => SetProperty(ref _portMonitor, value);
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Port
+        ///
+        /// <summary>
+        /// Gets or sets the configuration of the installing port.
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public PortConfig Port
+        {
+            get => _port;
+            set => SetProperty(ref _port, value);
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// PrinterDriver
+        ///
+        /// <summary>
+        /// Gets or sets the configuration of the installing printer
+        /// driver.
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public PrinterDriverConfig PrinterDriver
+        {
+            get => _printerDriver;
+            set => SetProperty(ref _printerDriver, value);
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Printer
+        ///
+        /// <summary>
+        /// Gets or sets the configuration of the installing printer.
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public PrinterConfig Printer
+        {
+            get => _printer;
+            set => SetProperty(ref _printer, value);
+        }
+
+        #endregion
+
+        #region Implementations
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// OnDeserializing
+        ///
+        /// <summary>
+        /// Occurs bofore deserializing.
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        [OnDeserializing]
+        private void OnDeserializing(StreamingContext context) => Reset();
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Reset
+        ///
+        /// <summary>
+        /// Resets values of fields.
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        private void Reset()
+        {
+            _portMonitor   = new PortMonitorConfig();
+            _port          = new PortConfig();
+            _printerDriver = new PrinterDriverConfig();
+            _printer       = new PrinterConfig();
+        }
+
+        #endregion
+
+        #region Fields
+        private PortMonitorConfig _portMonitor;
+        private PortConfig _port;
+        private PrinterDriverConfig _printerDriver;
+        private PrinterConfig _printer;
+        #endregion
+    }
+}
---
+++ Applications/Pinstaller/Core/Sources/Installer.cs
@@ -0,0 +1,161 @@
+﻿/* ------------------------------------------------------------------------- */
+//
+// Copyright (c) 2010 CubeSoft, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+/* ------------------------------------------------------------------------- */
+using Cube.DataContract;
+using Cube.FileSystem;
+using Cube.FileSystem.Mixin;
+using Cube.Generics;
+
+namespace Cube.Pdf.App.Pinstaller
+{
+    /* --------------------------------------------------------------------- */
+    ///
+    /// Installer
+    ///
+    /// <summary>
+    /// Provides functionality to install or uninstall the printer and
+    /// related devices.
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    public class Installer
+    {
+        #region Constructors
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Installer
+        ///
+        /// <summary>
+        /// Initializes a new instance of the DeviceConfig class with the
+        /// specified arguments.
+        /// </summary>
+        ///
+        /// <param name="format">Data format.</param>
+        /// <param name="src">Location of the serialized data.</param>
+        ///
+        /* ----------------------------------------------------------------- */
+        public Installer(Format format, string src) : this(format, src, new IO()) { }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Installer
+        ///
+        /// <summary>
+        /// Initializes a new instance of the DeviceConfig class with the
+        /// specified arguments.
+        /// </summary>
+        ///
+        /// <param name="format">Data format.</param>
+        /// <param name="src">Location of the serialized data.</param>
+        /// <param name="io">I/O handler.</param>
+        ///
+        /* ----------------------------------------------------------------- */
+        public Installer(Format format, string src, IO io)
+        {
+            Format   = Format;
+            Location = src;
+            IO       = io;
+            Config   = Create(format, src, io);
+        }
+
+        #endregion
+
+        #region Properties
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Config
+        ///
+        /// <summary>
+        /// Gets the configuration of the installing devices.
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public DeviceConfig Config { get; }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Format
+        ///
+        /// <summary>
+        /// Gets the data format of serialized data.
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public Format Format { get; }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Location
+        ///
+        /// <summary>
+        /// Gets the value that the serialized data is located.
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public string Location { get; }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// IO
+        ///
+        /// <summary>
+        /// Gets the I/O handler.
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public IO IO { get; }
+
+        #endregion
+
+        #region Implementations
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Create
+        ///
+        /// <summary>
+        /// Creates a new instance of the DeviceConfig class with the
+        /// specified arguments.
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        private static DeviceConfig Create(Format format, string src, IO io)
+        {
+            var dest = format == Format.Registry ?
+                       format.Deserialize<DeviceConfig>(src) :
+                       io.Load(src, e => format.Deserialize<DeviceConfig>(e));
+
+            if (dest == null) return dest;
+
+            var mon   = dest.PortMonitor;
+            var port  = dest.Port;
+            var drv   = dest.PrinterDriver;
+            var print = dest.Printer;
+
+            if (port  != null && !port.MonitorName.HasValue()) port.MonitorName = mon?.Name;
+            if (drv   != null && !drv.MonitorName.HasValue())  drv.MonitorName  = mon?.Name;
+            if (print != null && !print.PortName.HasValue())   print.PortName   = port?.Name;
+            if (print != null && !print.DriverName.HasValue()) print.DriverName = drv?.Name;
+
+            return dest;
+        }
+
+        #endregion
+    }
+}
