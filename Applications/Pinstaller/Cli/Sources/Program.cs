321783c4906d71d30db432e190dc03ce674a6c48
clown <tt.clown@gmail.com>
2018-11-06 15:22:36 +0900

    Console project for CubePinstaller.

---
+++ Applications/Pinstaller/Cli/Sources/Program.cs
@@ -0,0 +1,59 @@
+﻿/* ------------------------------------------------------------------------- */
+//
+// Copyright (c) 2010 CubeSoft, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+/* ------------------------------------------------------------------------- */
+using Cube.Log;
+using System;
+using System.Reflection;
+
+namespace Cube.Pdf.App.Pinstaller
+{
+    /* --------------------------------------------------------------------- */
+    ///
+    /// Program
+    ///
+    /// <summary>
+    /// Represents the main program.
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    static class Program
+    {
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Main
+        ///
+        /// <summary>
+        /// Executes the application.
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var type = typeof(Program);
+
+            try
+            {
+                Logger.Configure();
+                Logger.ObserveTaskException();
+                Logger.Info(type, Assembly.GetExecutingAssembly());
+                Logger.Info(type, $"Arguments:{string.Join(" ", args)}");
+            }
+            catch (Exception err) { Logger.Error(type, err.ToString()); }
+        }
+    }
+}
