7c9fa62f6b99c590266d41082d981b83c49b0d5f
clown <tt.clown@gmail.com>
2018-11-12 12:45:04 +0900

    Add tests.

--- Applications/Pinstaller/Tests/Sources/InstallerTest.cs
+++ Applications/Pinstaller/Tests/Sources/InstallerTest.cs
@@ -92,6 +92,20 @@ namespace Cube.Pdf.Tests.Pinstaller
 
         /* ----------------------------------------------------------------- */
         ///
+        /// Uninstall
+        ///
+        /// <summary>
+        /// Executes the test to uninstall devices.
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        [TestCase("SampleDummy.json")]
+        public void Uninstall(string filename) => Invoke(() =>
+            new Installer(Format.Json, GetExamplesWith(filename)).Uninstall()
+        );
+
+        /* ----------------------------------------------------------------- */
+        ///
         /// Install_Throws
         ///
         /// <summary>
