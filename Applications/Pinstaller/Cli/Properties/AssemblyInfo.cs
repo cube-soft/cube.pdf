c54ee2ce0cf44871b54d308e99c97ac1e935a609
clown <tt.clown@gmail.com>
2018-11-12 15:51:12 +0900

    Fix version number.

--- Applications/Pinstaller/Cli/Properties/AssemblyInfo.cs
+++ Applications/Pinstaller/Cli/Properties/AssemblyInfo.cs
@@ -2,8 +2,8 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.16")]
+[assembly: AssemblyFileVersion("1.0.0.16")]
 [assembly: AssemblyTitle("Cube PrinterInstaller")]
 [assembly: AssemblyDescription("Installing and uninstalling port monitor, port, printer driver, and printer.")]
 [assembly: AssemblyConfiguration("")]
