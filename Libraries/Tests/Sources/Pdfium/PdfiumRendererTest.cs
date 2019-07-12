4f40f55016a1b9b23d686d426274c2dc8462ac77
clown <tt.clown@gmail.com>
2019-07-12 15:22:34 +0900

    Update tests for PDFium.

--- Libraries/Tests/Sources/Pdfium/PdfiumRendererTest.cs
+++ Libraries/Tests/Sources/Pdfium/PdfiumRendererTest.cs
@@ -48,14 +48,17 @@ namespace Cube.Pdf.Tests.Pdfium
         ///
         /* ----------------------------------------------------------------- */
         [TestCaseSource(nameof(TestCases))]
-        public void Render(int id, string filename, int pagenum, int width, int height)
+        public void Render(int id, string filename, int pagenum, int width, int height, RenderOption options)
         {
             using (var src = new DocumentRenderer(GetSource(filename)))
-            using (var bmp = src.Render(src.GetPage(pagenum)))
             {
-                Assert.That(bmp.Width,  Is.EqualTo(width));
-                Assert.That(bmp.Height, Is.EqualTo(height));
-                bmp.Save(Get($"{nameof(Render)}-{id:D3}.png"), ImageFormat.Png);
+                src.RenderOption = options;
+                using (var bmp = src.Render(src.GetPage(pagenum)))
+                {
+                    Assert.That(bmp.Width, Is.EqualTo(width));
+                    Assert.That(bmp.Height, Is.EqualTo(height));
+                    bmp.Save(Get($"{nameof(Render)}-{id:D3}.png"), ImageFormat.Png);
+                }
             }
         }
 
@@ -69,12 +72,13 @@ namespace Cube.Pdf.Tests.Pdfium
         ///
         /* ----------------------------------------------------------------- */
         [TestCaseSource(nameof(TestCases))]
-        public void Render_Graphics(int id, string filename, int pagenum, int width, int height)
+        public void Render_Graphics(int id, string filename, int pagenum, int width, int height, RenderOption options)
         {
             using (var src = new DocumentRenderer(GetSource(filename)))
             using (var bmp = new Bitmap(width, height))
             using (var gs  = Graphics.FromImage(bmp))
             {
+                src.RenderOption = options;
                 src.Render(gs, src.GetPage(pagenum));
                 bmp.Save(Get($"{nameof(Render_Graphics)}-{id:D3}.png"), ImageFormat.Png);
             }
@@ -98,13 +102,18 @@ namespace Cube.Pdf.Tests.Pdfium
             get
             {
                 var n = 0;
-                yield return new TestCaseData(n++, "SampleRotation.pdf",   1, 595, 842);
-                yield return new TestCaseData(n++, "SampleRotation.pdf",   2, 842, 595);
-                yield return new TestCaseData(n++, "SampleRotation.pdf",   3, 595, 842);
-                yield return new TestCaseData(n++, "SampleRotation.pdf",   4, 842, 595);
-                yield return new TestCaseData(n++, "SampleImage.pdf",      1, 595, 842);
-                yield return new TestCaseData(n++, "SampleAlpha.pdf",      1, 595, 841);
-                yield return new TestCaseData(n++, "SampleAnnotation.pdf", 1, 595, 842);
+                yield return new TestCaseData(n++, "SampleRotation.pdf",   1, 595, 842, new RenderOption());
+                yield return new TestCaseData(n++, "SampleRotation.pdf",   2, 842, 595, new RenderOption());
+                yield return new TestCaseData(n++, "SampleRotation.pdf",   3, 595, 842, new RenderOption());
+                yield return new TestCaseData(n++, "SampleRotation.pdf",   4, 842, 595, new RenderOption());
+                yield return new TestCaseData(n++, "SampleImage.pdf",      1, 595, 842, new RenderOption());
+                yield return new TestCaseData(n++, "SampleImage.pdf",      1, 595, 842, new RenderOption { Print = true });
+                yield return new TestCaseData(n++, "SampleImage.pdf",      1, 595, 842, new RenderOption { Grayscale = true });
+                yield return new TestCaseData(n++, "SampleImage.pdf",      1, 595, 842, new RenderOption { AntiAlias = false });
+                yield return new TestCaseData(n++, "SampleImage.pdf",      1, 595, 842, new RenderOption { Background = Color.Black });
+                yield return new TestCaseData(n++, "SampleAlpha.pdf",      1, 595, 841, new RenderOption());
+                yield return new TestCaseData(n++, "SampleAnnotation.pdf", 1, 595, 842, new RenderOption { Annotation = true });
+                yield return new TestCaseData(n++, "SampleAnnotation.pdf", 1, 595, 842, new RenderOption { Annotation = false });
             }
         }
 
