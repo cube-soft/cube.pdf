147441a0135b00bcb46a15ae62dc1c09e41099e8
clown <tt.clown@gmail.com>
2022-03-11 16:50:35 +0900

    apply numeric-aware filename sorting.

---
+++ Tests/Pages/Sources/NumericAwareComparerTest.cs
@@ -0,0 +1,64 @@
+﻿/* ------------------------------------------------------------------------- */
+//
+// Copyright (c) 2013 CubeSoft, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+/* ------------------------------------------------------------------------- */
+using Cube.Collections;
+using NUnit.Framework;
+
+namespace Cube.Pdf.Pages.Tests
+{
+    /* --------------------------------------------------------------------- */
+    ///
+    /// NumericAwareComparerTest
+    ///
+    /// <summary>
+    /// Tests the NumericAwareComparer class.
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    [TestFixture]
+    class NumericAwareComparerTest
+    {
+        #region Tests
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Compare
+        ///
+        /// <summary>
+        /// Tests the Compare method.
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        [TestCase( "2",  "2", ExpectedResult =  0)]
+        [TestCase( "2",  "1", ExpectedResult =  1)]
+        [TestCase( "2", "01", ExpectedResult =  1)]
+        [TestCase( "2", "02", ExpectedResult =  2)] // >= 1
+        [TestCase( "2", "10", ExpectedResult = -1)]
+        [TestCase( "2", "20", ExpectedResult = -1)]
+        [TestCase( "2", "-2", ExpectedResult =  5)] // >= 1
+        [TestCase("02", "01", ExpectedResult =  1)]
+        [TestCase("02", "10", ExpectedResult = -1)]
+        [TestCase("1.2.3-alpha",  "1.2.3-beta", ExpectedResult = -1)]
+        [TestCase("1.2.13-alpha", "1.2.3-beta", ExpectedResult =  1)]
+        [TestCase("sample", "test",   ExpectedResult = -1)]
+        [TestCase("sample", "Sample", ExpectedResult = 32)] // >= 1
+        public int Compare(string x, string y) => new NumericAwareComparer().Compare(x, y);
+
+        #endregion
+    }
+}
