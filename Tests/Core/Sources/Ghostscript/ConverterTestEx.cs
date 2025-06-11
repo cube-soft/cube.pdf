ec63e4952671f57221e4518b1f61c217701125a2
clown <tt.clown@gmail.com>
2025-06-11 15:02:26 +0900

    Update Ghostscript 10.5.1

--- Tests/Core/Sources/Ghostscript/ConverterTestEx.cs
+++ Tests/Core/Sources/Ghostscript/ConverterTestEx.cs
@@ -50,8 +50,8 @@ class ConverterTestEx : FileFixture
     [Test]
     public void Revision()
     {
-        Assert.That(Converter.Revision, Is.EqualTo(10050));
-        Assert.That(Converter.Revision, Is.EqualTo(10050));
+        Assert.That(Converter.Revision, Is.EqualTo(10051));
+        Assert.That(Converter.Revision, Is.EqualTo(10051));
     }
 
     /* --------------------------------------------------------------------- */
